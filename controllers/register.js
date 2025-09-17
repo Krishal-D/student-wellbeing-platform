1ea5066c52e65935909283c85f13a1e398551d5b
Krishal-D <22151706@student.westernsydney.edu.au>
2025-09-17 17:12:59 +1000

    Added post method and improved css

--- controllers/register.js
+++ controllers/register.js
@@ -1,4 +1,33 @@
+let users = []
+
 
 export function showRegisterForm(req, res) {
   res.render('register', { title: 'Register' });
+}
+
+
+export function registerUser(req, res) {
+  const { name, email, password } = req.body
+
+  if (!name || !email || !password) {
+    return res.status(400).send('All fields are required')
+  }
+
+  const existingUser = users.find(u => u.email === email)
+  if (existingUser) {
+    return res.status(400).send('Email already registered')
+  }
+
+  const newUser = {
+    id: users.length + 1,
+    name,
+    email,
+    password 
+  }
+
+  users.push(newUser)
+
+  console.log('Registered users:', users)
+
+  res.redirect('/');
 }
