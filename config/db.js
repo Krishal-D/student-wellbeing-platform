53da21c0b2c82b3f4ea26641b0e48e83cf534d0d
Brendan Dickie <17975432@student.westernsydney.edu.au>
2025-09-29 02:10:26 +0000

    Add db.js file

---
+++ config/db.js
@@ -0,0 +1,22 @@
+// Code copied from lecture
+
+import postgres from 'postgres';
+
+const sql = postgres({
+    host: 'localhost',
+    port: 5432,
+    database: 'postgres',
+    username: 'postgres',
+    password: 'postgres'
+});
+export default sql;
+
+// Let test the connection and setup basic table
+(async () => {
+    try {
+        let result = await sql`SELECT NOW()`;
+        console.log(`connection was successful ${result[0].now}`);
+    } catch (error) {
+        console.error(`Failed to connect to the DB ${error}`);
+    }
+})();
