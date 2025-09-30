28023e9044cee78f5ae84b7881b9156f4406816c
ingcheav <22084048@student.westernsydney.edu.au>
2025-09-30 22:01:29 +1000

    added the post into the moodrouter

--- routes/mood.js
+++ routes/mood.js
@@ -2,3 +2,4 @@ import express from 'express';
 import * as mood from '../controllers/mood.js';
 export const moodRouter = express.Router();
 moodRouter.get('/mood', mood.show);
+moodRouter.post('/mood', mood.submit);
