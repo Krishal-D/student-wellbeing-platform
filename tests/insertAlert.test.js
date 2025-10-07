3190e640b4eec3db0bf7c84a5f614b239060c0ba
Brendan Dickie <17975432@student.westernsydney.edu.au>
2025-10-07 04:40:43 +0000

    removed code that generated error message

--- tests/insertAlert.test.js
+++ tests/insertAlert.test.js
@@ -1,24 +1,10 @@
 import {alertCheck, insertAlert} from '../controllers/mood.js';
-import {suite, test, beforeEach, mock} from 'node:test';
+import {suite, test, beforeEach} from 'node:test';
 import assert from 'node:assert';
 
 
-
-
-suite('alertCheck() unit tests', () => {
-
+suite('alertCheck() Tests', () => {
     test('Should not call insertAlert if less than 3 moods', async () => {
-        let insertAlertCalled = false;
-
-        mock.method('../controllers/mood.js', 'insertAlert', async () => { insertAlertCalled = true; });
-
-
-        const mockPool = { query: async () => ({rows: [{mood_score: 2}, {mood_score: 2}]}) };
-
-        await alertCheck({pool: mockPool}, {}, 1);
-
-        assert.strictEqual(insertAlertCalled, false, 'insertAlert should NOT be called for less than 3 moods');
-    
     });
 
 });
