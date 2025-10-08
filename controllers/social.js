8f5b98fe1567e7d5089848778c814ce08e440258
Brendan <17975432@student.westernsydney.edu.au>
2025-10-08 05:56:36 +0000

    Add partial form validation for social page

--- controllers/social.js
+++ controllers/social.js
@@ -1,23 +1,41 @@
 import {pool} from '../config/db.js';
 
 export async function show(req, res, next) {
-  res.render('social', { title: 'Social Networking' });
+  res.render('social', { title: 'Social Networking', errors : {} });
 }
 
 export async function submit(req, res, next) {
+
+  const user_id = req.user.userId;
+  const {to_user, message_text} = req.body;
+  const errors = {};
+
+  // if username field is empty
+  if (!to_user) {
+    errors.to_user = 'No username entered.';
+  }
+  // if message field is empty
+  if (!message_text) {
+    errors.message_text = 'No message entered.';
+  }
+
   try {
-    const user_id = req.user.userId;
-
-    const {to_user, message_text} = req.body;
-
-    // Check message is not to long
-    if (message_text.length > 5000) {
-      return res.status(400).send('Message is too long');
-    }
+    const result = await pool.query(
+      'SELECT id FROM users WHERE name = $1', [to_user]
+    );
+    const userRow = result.rows; 
+    console.log("User row: ", userRow);
+    console.log("User row length: ", userRow.length);
 
   } catch (err) {
       console.error('Database error: ', err);
-      res.status(500).send('Server error');
+      res.status(500).send('Database error');
   } 
+
+  if (Object.keys(errors).length > 0) {
+    return res.render('social', { title: 'Social Networking', errors: errors });
+  }
+
+
 }
 
