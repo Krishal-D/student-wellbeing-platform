d5c88cba3e97da3189d7c6325450e4586e689760
Brendan Dickie <17975432@student.westernsydney.edu.au>
2025-10-09 08:26:49 +0000

    Finish javascript for social networking feature

--- controllers/social.js
+++ controllers/social.js
@@ -1,7 +1,36 @@
 import {pool} from '../config/db.js';
 
+let inboxMessages; // used in both show() and submit()
+
 export async function show(req, res, next) {
-  res.render('social', { title: 'Social Networking', errors : {}, success : {} });
+  try {
+    const user_id = req.user.userId;
+    
+    // get all messages that have been sent
+    const result = await pool.query(
+      `SELECT users.name, message.message_text, message.created_at
+      FROM message 
+      JOIN users ON message.to_user_id = users.id 
+      WHERE message.to_user_id = $1
+      ORDER BY message.created_at DESC`,
+      [user_id]
+    );
+
+    // convert to AU/Sydney timezone
+    inboxMessages = result.rows.map((message) => {
+      const localTime = new Date(message.created_at).toLocaleString('en-AU', {timeZone: 'Australia/Sydney'});
+      return {...message, created_at: localTime};
+    });
+
+
+    res.render('social', { title: 'Social Networking', inboxMessages, errors : {}, success : false });
+
+  } catch (err) {
+      console.error('Database error when getting messages from DB for inbox', err);
+      res.status(500).send('Database error when getting messages from DB for inbox');
+  }
+
+  
 }
 
 export async function submit(req, res, next) {
@@ -9,56 +38,58 @@ export async function submit(req, res, next) {
   const user_id = req.user.userId;
   const {to_user, message_text} = req.body;
   const errors = {};
-  const success = {}
   let userRow;
 
   // if username field is empty
   if (!to_user) {
     errors.to_user = 'No username entered.';
+  } else {
+    // check if the username exists in the database
+    try {
+      const result = await pool.query(
+        `SELECT id, name FROM users WHERE name = $1`, [to_user]
+      );
+      userRow = result.rows; 
+
+      if (userRow.length == 0) {
+        errors.to_user = "User not found in database";
+      }
+
+      // console.log('userRow length: ', userRow.length);
+
+    } catch (err) {
+        console.error('Database error when reading from database', err);
+        errors.to_user = 'The username does not exist.';
+        return res.render('social', { title: 'Social Networking', inboxMessages, errors: errors, success : false });
+    }
   }
+
+
+
   // if message field is empty
   if (!message_text) {
     errors.message_text = 'No message entered.';
-  } else if (message_text.length > 5000) {
-    errors.message_text = 'Message not exceed 5000 characters';
+  } else if (message_text.length > 1000) {
+    errors.message_text = 'Message not exceed 1000 characters';
   }
 
-  // check if the username exists in the database
-  try {
-    const result = await pool.query(
-      `SELECT id, name FROM users WHERE name = $1`, [to_user]
-    );
-    userRow = result.rows; 
-
-    if (userRow.length == 0) {
-      errors.to_user = "User not found in database";
-    }
-
-    // console.log('userRow length: ', userRow.length);
-
-  } catch (err) {
-      console.error('Database error when reading from database', err);
-      errors.to_user = 'The username does not exist.';
-      return res.render('social', { title: 'Social Networking', errors: errors });
-  }
-
-  // if there are errors, render page with error messages under form elements
+  // if there are errors: render page with error messages under form elements
+  // else: insert message into database
   if (Object.keys(errors).length > 0) {
-    return res.render('social', { title: 'Social Networking', errors: errors });
-  } else { // insert message into database
+    return res.render('social', { title: 'Social Networking', inboxMessages, errors: errors, success : false });
+  } else { 
     try {
       await pool.query(
           `INSERT INTO message 
           (to_user_id, from_user_id, message_text) 
           VALUES ($1, $2, $3)`,
           [userRow[0].id, user_id, message_text]
-          // ^ recipitent, signed-in user, message content
+          // ^ recipient, signed-in user, message content
       ); 
 
       console.log('message entered into the message table');
 
-      success.submitMessage = "Message successfully sent."
-      return res.render('social', { title: 'Social Networking', errors: errors, success: success});
+      return res.render('social', { title: 'Social Networking', inboxMessages, errors: errors, success: true });
       
     } catch (err) {
       console.error('Database error when entering message into database', err);
@@ -67,9 +98,5 @@ export async function submit(req, res, next) {
 
   }
  
-
-  
-
-
 }
 
--- migrations/20251008_1_add_message_table.sql
+++ migrations/20251008_1_add_message_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS message (
     id SERIAL PRIMARY KEY,
     to_user_id INTEGER REFERENCES users(id),
     from_user_id INTEGER REFERENCES users(id),
-    message_text VARCHAR(5000),
+    message_text VARCHAR(1000),
     created_at TIMESTAMPTZ DEFAULT NOW()
 );
