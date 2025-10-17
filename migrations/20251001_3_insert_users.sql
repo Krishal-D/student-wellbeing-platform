97848bfecae6395fea3beeb6aeb3bd3cca667e1d
Krishal-D <22151706@student.westernsydney.edu.au>
2025-10-17 00:47:52 +0000

    set Testuser passwords to password1/password2 (bcrypt)

--- migrations/20251001_3_insert_users.sql
+++ migrations/20251001_3_insert_users.sql
@@ -1,6 +1,7 @@
+-- password1 / password2 hashed with bcrypt 10 rounds
 INSERT INTO users (name, email, password) VALUES
-('Testuser1', 'testuser1@mail.com', '$2b$10$FMtzZtIAc/odbhNkWVzCjOnN7daXy/2h36hh/kBk1xpdIDUYLsJeK')
+('Testuser1', 'testuser1@mail.com', '$2b$10$ZLIRub9pOb/mRXT.J9RjqOSjJ2XIFkcZTKi7bQp3g2BIpH/XnrCIq')
 ON CONFLICT (email) DO NOTHING;
 INSERT INTO users (name, email, password) VALUES
-('Testuser2', 'testuser2@mail.com', '$2b$10$o0T6a6ve/AMQG9L.ekaAfOgLvWqU/TTzv5VD/zQb.mXl7wUP2JFOK')
+('Testuser2', 'testuser2@mail.com', '$2b$10$Gf.fYRYgemfmO5Sei3gqy.li/BnCEPvQ4cxAB4wOKSaRnmScF3UXy')
 ON CONFLICT (email) DO NOTHING;
