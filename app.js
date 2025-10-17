9bd7a23a7374c0d3d3ff0eb6bdb3a391296b8712
ingcheav <22084048@student.westernsydney.edu.au>
2025-10-17 11:10:55 +1100

    delete it first to avoid the conflict

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ import { authRouter } from './routes/auth.js';
 
 import { requireAuth, addUserToViews } from './middleware/auth.js';
 import { socialRouter } from './routes/social.js';
-import { gamificationRouter } from './routes/gamification.js';
+
 
 // Setup debug module to spit out all messages
 // Do `npn start` to see the debug messages
@@ -60,7 +60,7 @@ app.use('/', alertDashboardRouter);
 app.use('/', usersRouter);
 app.use('/', historyRouter);
 app.use('/', socialRouter);
-app.use('/', gamificationRouter);
+
 
 // Not encouraged, but this is a simple example of how to register a route without a router.
 app.get('/test', (req, res) => {
